52d20d1abec71ab9531753e5efdf6c87d7278244
caiohenrique-developer <caiorussellrox@msn.com>
2022-03-22 16:49:55 -0300

    📈 [wip]: Creating Message and MessageList component

---
+++ src/components/Message/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {
+  View,
+  Text
+} from 'react-native';
+import { UserPhoto } from '../UserPhoto';
+
+import { styles } from './styles';
+
+export function Message(){
+  return (
+    <View style={styles.container}>
+        <Text style={styles.message}>Texto da mensagem</Text>
+
+        <View style={styles.footer}>
+          <UserPhoto imageUri="https://github.com/caiohenrique-developer.png" sizes="SMALL" />
+
+          <Text style={styles.userName}>Nome do usuário</Text>
+        </View>
+    </View>
+  );
+}
