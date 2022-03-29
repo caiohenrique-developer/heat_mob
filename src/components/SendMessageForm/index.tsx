bca08690f7605b257bcb444fd8af627f633f3abe
caiohenrique-developer <caiorussellrox@msn.com>
2022-03-29 19:14:01 -0300

    ✨ [feat]: Send message by app

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,18 +1,39 @@
 import React, { useState } from 'react';
 
 import {
+  Alert,
+  Keyboard,
     TextInput,
   View
 } from 'react-native';
+import { api } from '../../services/api';
 import { COLORS } from '../../theme';
 import { Button } from '../Button';
 
 import { styles } from './styles';
 
 export function SendMessageForm(){
-    const [message, setMessage] = useState('');
-    const [sendingMessage, setSendingMessage] = useState(false);
-    
+  const [message, setMessage] = useState('');
+  const [sendingMessage, setSendingMessage] = useState(false);
+
+  async function handleMessageSubmit() {
+    const messageFormatted = message.trim();
+
+    if(messageFormatted.length > 0) {
+      setSendingMessage(true);
+
+      await api.post('/messages', { message: messageFormatted });
+
+      setMessage('');
+      setSendingMessage(false);
+      Keyboard.dismiss();
+
+      Alert.alert('Mensagem enviada com sucesso!');
+    } else {
+      Alert.alert('Digite uma mensagem a ser enviada!');
+    }
+  }
+  
   return (
     <View style={styles.container}>
         <TextInput
@@ -30,6 +51,8 @@ export function SendMessageForm(){
             title="ENVIAR MENSAGEM"
             backgroundColor={COLORS.PINK}
             color={COLORS.WHITE}
+            onPress={handleMessageSubmit}
+            isLoading={sendingMessage}
         />
     </View>
   );
